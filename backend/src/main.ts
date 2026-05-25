61d76d68f8ebb57e28729747dcfe0ceaabd2ffe2
PROTTOY SARKER DIGANTO <23-50812-1@student.aiub.edu>
2026-05-26 02:21:04 +0600

    Fix production CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,11 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://prottoysarkerdiganto.dev',
-      'https://www.prottoysarkerdiganto.dev',
-    ],
+    origin: true,
 
     credentials: true,
   });
